2bc72de897f112eb6817cc54dcce09c2de00fb17
dn757657 <daniel.js.campbell@gmail.com>
2022-08-23 18:04:40 -0300

    source/assembly paradigm early structural design
    some elements of sources only paradigm reused, new paradigm of function assembly with execution at end - much less code, recursive sources to form assets/assemblies/models

--- assembly.py
+++ assembly.py
@@ -1,139 +1,40 @@
+from typing import List, Dict, TypedDict
+from collections.abc import Callable
 import pandas as pd
-import numpy as np
-import logging
+import datetime as dt
+from processing import process, func_runner, df_condenser
 
-class Assembler:
-    """ assemble dataframes """
 
-    def __init__(self):
+sample_func = Callable[[dt.datetime, dt.datetime], pd.DataFrame]
+
+
+class Source:
+
+    def __init__(self,
+                 raw_sources: List[sample_func],
+                 forecast: sample_func,
+                 interpolator: sample_func):
+        # inputs
+        self.raw_sources = raw_sources
+        self.forecast = forecast
+        self.interpolator = interpolator
+
+        # internal
+        self._assembly = list()
+
+    def assemble(self):
+        self._assembly.append(self.raw_sources)
+        self._assembly.append(self.forecast)
+        self._assembly.append(self.interpolator)
+
         pass
 
-    def assembler(
-            self,
-            subs: list,
-            out_lbl='out'
-    ):
-        """
-        assemble groups of dataframes into the same index using fillna in order of list priority
-        :param subs:
-        :param to:
-        :param out_lbl:
-        :return:
-        """
+    def process(self, start, end):
+        sampled = process(self._assembly, func_runner, start, end)
+        processed = process(sampled, df_condenser)
 
-        # combine requested index for assembly from all subs
-        to_list = flatten(subs)
-        to_idx = pd.DatetimeIndex(next(to_list).index)
-        to_idx = [to_idx.union(x.index) for x in to_list][0]
+        # TODO process mods funcs, functions to modify processed data, df in df out
+        # TODO assemble everything as functions and execute at endd
+        # TODO incorporate samplefuncs into forecasting? samplefunc as training
 
-        self.dummy_drill(subs=subs)
-
-        return assembly_df
-
-    def dummy_assembler(self, subs):
-        # while any(isinstance(x, list) for x in subs):
-        subs = self.dummy_drill(subs=subs)
-
-        final = self.dummy_assemble(suba=subs)
-        return final
-
-    # def condense(self, condense_list, condensor_func):
-    #     """
-    #     condense all lists in condense list using the condensor function
-    #     :param condense_list:       list of components or lists of any level to be processed by condensor func
-    #     :param condensor_func:      function that takes a list of items and returns a single item
-    #     :return:                    condense list fully condensed to single item
-    #     """
-    #
-    #     while any(isinstance(x, list) for x in condense_list):
-    #         for i, sub in enumerate(condense_list):
-    #             if isinstance(sub, list):
-    #                 if any(isinstance(x, list) for x in sub):
-    #                     # recurse for each sub list until no lists
-    #                     self.condense(condense_list=sub, condensor_func=condensor_func)
-    #                 else:
-    #                     condensor_func(condense_list)
-    #                     break
-    #
-    #     return condense_list
-
-    def dummy_assemble(self, suba):
-        dummy = 0
-        for sub in suba:
-            dummy += sub
-
-        return dummy
-
-    def assemble(
-            self,
-            subs,
-            to,
-            out_lbl='out',
-    ):
-
-        assembly_df = pd.DataFrame({'index': to, out_lbl: np.nan})
-        assembly_df.set_index('index', drop=True, inplace=True)
-
-        # fill in list order of priority
-        # [{df:, lbl:,}, {df:, lbl:,}]
-        for sub in subs:
-            # get df from passed with lbl only
-            assembly_df = pd.merge(assembly_df,
-                                   sub,
-                                   how='outer',
-                                   right_index=True,
-                                   left_index=True
-                                   )
-            assembly_df.fillna(assembly_df[sub.columns[0]], inplace=True)
-
-        if assembly_df[out_lbl].isnull().values.any():
-            logging.error(f"Cannot create full assembly from sub-assembly: \n{subs}")
-
-        return [{'df': assembly_df, 'lbl': out_lbl}]
-
-    def df_condenser(self, df_list):
-        """ merge list of dataframes in native list order """
-
-        # combine requested index for assembly from all dfs
-        to_list = flatten(df_list)
-        to_idx = pd.DatetimeIndex(next(to_list).index)
-        to_idx = [to_idx.union(x.index) for x in to_list][0]
-
-
-
-        return
-
-
-from collections.abc import Iterable
-
-
-def flatten(xs):
-    for x in xs:
-        if isinstance(x, Iterable) \
-                and not isinstance(x, (str, bytes)) \
-                and not isinstance(x, pd.DatetimeIndex) \
-                and not isinstance(x, pd.DataFrame):
-            yield from flatten(x)
-        else:
-            yield x
-
-
-def condense(condense_list, condenser_func):
-    """
-    condense all lists in condense list using the condenser function
-    :param condense_list:       list of components or lists of any level to be processed by condenser func
-    :param condenser_func:      function that takes a list of items and returns a single item
-    :return:                    condense list fully condensed to single item
-    """
-
-    while any(isinstance(x, list) for x in condense_list):
-        for i, sub in enumerate(condense_list):
-            if isinstance(sub, list):
-                if any(isinstance(x, list) for x in sub):
-                    # recurse for each sub list until no lists
-                    condense(condense_list=sub, condenser_func=condenser_func)
-                else:
-                    condenser_func(condense_list)
-                    break
-
-    return condense_list
+        return processed
--- test.py
+++ test.py
@@ -19,23 +19,25 @@ import autots.models.statsmodels as statsmodels
 from kats.models.prophet import ProphetModel, ProphetParams
 from kats.consts import TimeSeriesData
 from dn_qtrade.questrade_2 import QTAccount
-from assembly import Assembler
+from processing import process, df_condenser, flatten
 from prod_models import banking_asset_prod
 from interpolation import interp_zero
 
 from sources import YahooMarket, DbSource, Periodic
 from forecast import KatsProphet, Static, Source, ZeroForecast
 from model import FinModel
-
+from interpolation import interp_zero
+import numpy as np
+import copy
 
 # # get banking database models and init connection
 # db = BplModel()
 # session = Session(db.engine)
 
 # SAMPLING DATES
-start = datetime.datetime.strptime('2020-01-01', '%Y-%m-%d')
+start = datetime.datetime.strptime('2021-01-01', '%Y-%m-%d')
 trans = datetime.datetime.strptime('2021-04-13', '%Y-%m-%d')
-end = datetime.datetime.strptime('2021-10-01', '%Y-%m-%d')
+end = datetime.datetime.strptime('2022-10-01', '%Y-%m-%d')
 now = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 fcst = datetime.datetime.strptime('2023-07-08', '%Y-%m-%d')
 
@@ -65,12 +67,55 @@ def assembly_testing():
     #                              forecast=Source(name=food_budget_src.name, source=food_budget_src),
     #                              cumulative=True,
     #                              duplicate_indices='sum')
-
+    #
     # subs = [food_spending_src.sample(start, end)[[food_spending_src.lbls['base']]],
     #         food_spending_src.sample(start, end)[[food_spending_src.lbls['fcst']]]]
+    #
+    # # combine requested index for assembly from all dfs
+    # to_list = flatten(subs)
+    # to_idx = pd.DatetimeIndex(next(to_list).index)
+    # to_idx = [to_idx.union(x.index) for x in to_list][0]
+    #
+    # idx_df = pd.DataFrame({'index': to_idx, 'rando': np.nan})
+    # idx_df.set_index('index', drop=True, inplace=True)
+    #
+    # sample_df = pd.DataFrame({'index': sample_at1, 'rando': np.nan})
+    # sample_df.set_index('index', drop=True, inplace=True)
+    #
+    # all_idx_df = pd.merge(sample_df, idx_df, right_index=True, left_index=True, how='outer')
+    #
+    # subs.append(all_idx_df)
+    # # subs.append(sample_df)
+    # # could also interp future seperately? would need to ensure interp is confined to each
+    # # individual sample where its applied?
+    # subs.append(interp_zero(copy.copy(pd.merge(subs[0], all_idx_df, right_index=True, left_index=True, how='outer')), food_spending_src.lbls['base']))
 
     subs = [[[1, 2, 3], [1, 2, 3]], [1, 2, 3]]
-    a = Assembler().dummy_assembler(subs=subs)
+    a = flatten(subs, mod_func=addemup)
+    a = list(a)
+
+    a = process(subs, df_condenser)
+
+    # subs2 = [a, interp_zero(a, a.columns[0])]
+
+    return
+
+def addemup(xs):
+    y = 0
+    for x in xs:
+        y += x
+
+    return y
+# maybe a general py utils file is in store
+# from collections.abc import Iterable
+# def flatten(xs):
+#     for x in xs:
+#         if isinstance(x, Iterable) and not isinstance(x, pd.DatetimeIndex) and not isinstance(x, pd.DataFrame)\
+#                 and not isinstance(x, (str, bytes)):
+#             yield from flatten(x)
+#         else:
+#             yield x
+
 
 def main():
     assembly_testing()
